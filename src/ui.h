fa35e05b20156fe6981bd3fd911c5bb77dad2067
Musab Shakeel <musabshakeel@gmail.com>
2022-05-25 11:12:22 -0400

    major refactor complete (used arrays to store info about the 3 planes); all 3 plane slice views now work

--- src/ui.h
+++ src/ui.h
@@ -20,8 +20,6 @@
 #include <vtkColorTransferFunction.h>
 #include <vtkVolumeProperty.h>
 
-
-
 // Qt header files
 #include <QMainWindow.h>
 #include <QLayout.h>
@@ -37,100 +35,106 @@
 #include <QMenuBar.h>
 #include <QMenu.h>
 
+
 // Class that represents the main window for our application
-class ui : public QMainWindow
-{
+class ui : public QMainWindow {
+
 	Q_OBJECT
 public:
+	// constants
+	int VOLUME = 0;
+	int AXIAL = 1;    // xy- (setElement is 2,3)
+	int CORONAL = 2;  // x-z (setElement is 1,3)
+	int SAGITTAL = 3; // -yz (setElement is 0,1)
 
-	QSlider* slider_x, *slider_y, *slider_z;
+	static const int NUM_VIEWPORTS = 4;
 
-	vtkSmartPointer<vtkGenericOpenGLRenderWindow> window_volume, window_x, window_y, window_z;
+	QSlider* slider_arr[NUM_VIEWPORTS]; // 0th element is blank (3 sliders but 4 viewports)
+	QLabel* slider_label_arr[NUM_VIEWPORTS];
 
-	QLabel* label_slice_x, *label_slice_y, *label_slice_z;
+	QVTKOpenGLNativeWidget* viewport_arr[NUM_VIEWPORTS];
+	vtkSmartPointer<vtkGenericOpenGLRenderWindow> window_arr[NUM_VIEWPORTS];
 
-	vtkSmartPointer<vtkMatrix4x4> reslice_axes;
-	vtkSmartPointer<vtkImageReslice> reslice;
-	vtkSmartPointer<vtkImageActor> iactor;
-	vtkSmartPointer<vtkRenderer> renderer;
-
-	vtkSmartPointer<vtkMatrix4x4> reslice_axes_y;
-	vtkSmartPointer<vtkImageReslice> reslice_y;
-
-	vtkSmartPointer<vtkImageActor> iactor_y;
-	vtkSmartPointer<vtkRenderer> renderer_y;
+	vtkSmartPointer<vtkMatrix4x4> reslice_axes_arr[4];
+	vtkSmartPointer<vtkImageReslice> reslice_arr[4];
+	vtkSmartPointer<vtkImageActor> iactor_arr[4];
+	vtkSmartPointer<vtkRenderer> renderer_arr[4];
 
 	bool is_data_loaded = false;
+
+	// matrices that defines planes for slicing
+	// https://public.kitware.com/pipermail/vtkusers/2016-January/093908.html
+	double axial_plane[16] = {
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1 };
+
+	double coronal_plane[16] = {
+		1, 0, 0, 0,
+		0, 0, 1, 0,
+		0, -1, 0, 0,
+		0, 0, 0, 1 };
+
+	double sagittal_plane[16] = {
+		0, 0, -1, 0,
+		1, 0, 0, 0,
+		0, -1, 0, 0,
+		0, 0, 0, 1 };
+
+	// summarize above in an array of int arrays
+	double* plane_arr[4] = { NULL, axial_plane, coronal_plane, sagittal_plane };
 
 
 	// Constructor (happens when created)
 	ui() {
 
+
 		// S==================== METADATA ==================== //
-		
+
 		// Resize the window
 		this->resize(1200, 400);
 
 		this->setWindowTitle("DICOM Reader");
 
 		// S======================= WIDGETS ======================= //
-		
+
 		// Create the "central" (primary) widget for the window
 		QWidget* widget = new QWidget();
 
-		slider_x = new QSlider();
-		slider_x->setOrientation(Qt::Vertical);
+		// initialize sliders for each of the 3 slice planes
+		for (int i = 1; i < NUM_VIEWPORTS; i++) {
+			slider_arr[i] = new QSlider();
+			slider_arr[i]->setOrientation(Qt::Vertical);
+		}
 
-		slider_y = new QSlider();
-		slider_y->setOrientation(Qt::Vertical);
+		// initialize Qt viewports and VTK render windows
+		for (int i = 0; i < NUM_VIEWPORTS; i++) {
+			// initialize Qt viewports (that will show VTK render window)
+			viewport_arr[i] = new QVTKOpenGLNativeWidget();
+			viewport_arr[i]->enableHiDPI();
 
-		slider_z = new QSlider();
-		slider_z->setOrientation(Qt::Vertical);
+			// initialize VTK render windows
+			window_arr[i] = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
 
-		// QT's VTK viewports
-		QVTKOpenGLNativeWidget* viewport_volume = new QVTKOpenGLNativeWidget();
-		QVTKOpenGLNativeWidget* viewport_x = new QVTKOpenGLNativeWidget();
-		QVTKOpenGLNativeWidget* viewport_y = new QVTKOpenGLNativeWidget();
-		QVTKOpenGLNativeWidget* viewport_z = new QVTKOpenGLNativeWidget();
-		
-		// VTK's special render windows
-		window_volume = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
-		window_x = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
-		window_y = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
-		window_z = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+			// set QT viewports' render windows to the VTK render windows
+			viewport_arr[i]->SetRenderWindow(window_arr[i]);
 
-		// support for high DPI screens
-		viewport_volume->enableHiDPI();
-		viewport_x->enableHiDPI();
-		viewport_y->enableHiDPI();
-		viewport_z->enableHiDPI();
+			// set a minimum size for the viewports
+			viewport_arr[i]->setMinimumSize(400, 400);
 
-		// set QT viewports' render windows to the VTK render windows
-		viewport_volume->SetRenderWindow(window_volume);
-		viewport_x->SetRenderWindow(window_x);
-		viewport_y->SetRenderWindow(window_y);
-		viewport_z->SetRenderWindow(window_z);
+		}
 
-		// set a minimum size for the viewports
-		viewport_volume->setMinimumSize(400, 400);
-		viewport_x->setMinimumSize(400, 400);
-		viewport_y->setMinimumSize(400, 400);
-		viewport_z->setMinimumSize(400, 400);
-		
+		// initialize slice labels for the 3 planes
+		for (int i = 1; i < NUM_VIEWPORTS; i++) {
+			slider_label_arr[i] = new QLabel("#");
+			slider_label_arr[i]->setFixedSize(60, 20);
+		}
+
 		/* Add a menu item with one action to QMainWindow's in-built menu bar */
 		QAction* load_action = new QAction("Load DICOM data");
 		auto fileMenu = menuBar()->addMenu("&File");
 		fileMenu->addAction(load_action);
-
-		/* Slice labels */
-		label_slice_x = new QLabel("#");
-		label_slice_x->setFixedSize(60, 20);
-
-		label_slice_y = new QLabel("#");
-		label_slice_y->setFixedSize(60, 20);
-
-		label_slice_z = new QLabel("#");
-		label_slice_z->setFixedSize(60, 20);
 
 
 		// S================ CREATE LAYOUTs ================ //
@@ -138,9 +142,12 @@ public:
 		QHBoxLayout* layout_row0 = new QHBoxLayout();
 		QHBoxLayout* layout_row1 = new QHBoxLayout();
 
-		QVBoxLayout* layout_label_slice_x = new QVBoxLayout();
-		QVBoxLayout* layout_label_slice_y = new QVBoxLayout();
-		QVBoxLayout* layout_label_slice_z = new QVBoxLayout();
+		QVBoxLayout* layout_label_slice_arr[4];
+
+		// initialize layouts for slice labels for the 3 planes
+		for (int i = 1; i < NUM_VIEWPORTS; i++) {
+			layout_label_slice_arr[i] = new QVBoxLayout();
+		}
 
 
 		// S========== ADD WIDGETS/LAYOUTS TO LAYOUTs ========= //
@@ -150,42 +157,42 @@ public:
 		layout_vertical_main->addLayout(layout_row0);
 		layout_vertical_main->addLayout(layout_row1);
 
+		// populate row0
 		layout_row0->addSpacing(22); // no slider for volume view
-		layout_row0->addWidget(viewport_volume);
-		layout_row0->addWidget(slider_x);
-		layout_row0->addWidget(viewport_x);
+		layout_row0->addWidget(viewport_arr[VOLUME]);
 
-		viewport_x->setLayout(layout_label_slice_x);
-		layout_label_slice_x->addWidget(label_slice_x);
-		layout_label_slice_x->addStretch();
+		layout_row0->addWidget(slider_arr[AXIAL]);
+		layout_row0->addWidget(viewport_arr[AXIAL]);
 
-		viewport_y->setLayout(layout_label_slice_y);
-		layout_label_slice_y->addWidget(label_slice_y);
-		layout_label_slice_y->addStretch();
+		// populate row1
+		layout_row1->addWidget(slider_arr[CORONAL]);
+		layout_row1->addWidget(viewport_arr[CORONAL]);
 
-		viewport_z->setLayout(layout_label_slice_z);
-		layout_label_slice_z->addWidget(label_slice_z);
-		layout_label_slice_z->addStretch();
+		layout_row1->addWidget(slider_arr[SAGITTAL]);
+		layout_row1->addWidget(viewport_arr[SAGITTAL]);
 
-		layout_row1->addWidget(slider_y);
-		layout_row1->addWidget(viewport_y);
-		layout_row1->addWidget(slider_z);
-		layout_row1->addWidget(viewport_z);
-		
-		
+		// populate slider labels for each of 3 planes
+		for (int i = 1; i < NUM_VIEWPORTS; i++) {
+			viewport_arr[i]->setLayout(layout_label_slice_arr[i]);
+			layout_label_slice_arr[i]->addWidget(slider_label_arr[i]);
+			layout_label_slice_arr[i]->addStretch();
+		}
+
 
 		// S========== CONNECT SIGNALS TO SLOTS ========= //
+		// file menu: load DICOM dataset action
 		connect(load_action, SIGNAL(triggered()), this, SLOT(load_DICOM()));
-		connect(slider_x, SIGNAL(valueChanged(int)), this, SLOT(sliderx_changed(int)));
-		connect(slider_y, SIGNAL(valueChanged(int)), this, SLOT(slidery_changed(int)));
-		//connect(slider_z, SIGNAL(valueChanged(int)), this, SLOT(sliderx_changed(int)));
+
+		connect(slider_arr[AXIAL], SIGNAL(valueChanged(int)), this, SLOT(slider_changed(int)));
+		connect(slider_arr[CORONAL], SIGNAL(valueChanged(int)), this, SLOT(slider_changed(int)));
+		connect(slider_arr[SAGITTAL], SIGNAL(valueChanged(int)), this, SLOT(slider_changed(int)));
 
 		// Display the window
 		this->show();
 	}
 
 	/*
-	Pop up a dialog window that allows user to choose a directory. Returns a QDir object containing 
+	Pop up a dialog window that allows user to choose a directory. Returns a QDir object containing
 	the absolute path to the user-specified data directory.
 	*/
 	QDir choose_directory() {
@@ -197,7 +204,7 @@ public:
 			| QFileDialog::DontResolveSymlinks);
 
 		QTextStream qt_cout(stdout);
-		
+
 		// QDir current_dir = QDir(QDir::currentPath());
 		//qt_cout << current_dir.relativeFilePath(dir.absolutePath());
 
@@ -205,12 +212,16 @@ public:
 
 	}
 
-	void load_DICOM_image_x(QDir dicom_dir) {
+	void load_DICOM_image(QDir dicom_dir, int plane_idx) {
+
+		// rmb, 0 is a dummy idx to account for the volume viewport
+		if (plane_idx == 0) {
+			cout << "umm plane_idx should never be 0\n";
+			return;
+		}
 
 		is_data_loaded = false;
 		cout << "loading data\n";
-
-		//QDir dicom_dir = choose_directory();
 
 		// Read all the DICOM files in the specified directory.
 		vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
@@ -224,120 +235,45 @@ public:
 		int* dims = reader->GetOutput()->GetDimensions(); // Get the data dimensions
 		double* range = reader->GetOutput()->GetScalarRange(); // Get the range of intensity values
 
-		// Create a matrix which defines a plane. This plane will be used to "slice" the data
-		static double axial[16] = {
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1 };
-
-		reslice_axes = vtkSmartPointer<vtkMatrix4x4>::New();
-		reslice_axes->DeepCopy(axial);
+		reslice_axes_arr[plane_idx] = vtkSmartPointer<vtkMatrix4x4>::New();
+		reslice_axes_arr[plane_idx]->DeepCopy(plane_arr[plane_idx]);
 
 		// vtkImageReslice is the filter that does the slicing (slices a 3D dataset to become 2D)
-		reslice = vtkSmartPointer<vtkImageReslice>::New();
-		reslice->SetInputConnection(reader->GetOutputPort()); // connect the reader to this filter
-		reslice->SetOutputDimensionality(2);
-		reslice->SetResliceAxes(reslice_axes); // tell it what plane to slice with
-		reslice->SetInterpolationModeToLinear();
-		reslice->Update();
+		reslice_arr[plane_idx] = vtkSmartPointer<vtkImageReslice>::New();
+		reslice_arr[plane_idx]->SetInputConnection(reader->GetOutputPort()); // connect the reader to this filter
+		reslice_arr[plane_idx]->SetOutputDimensionality(2);
+		reslice_arr[plane_idx]->SetResliceAxes(reslice_axes_arr[plane_idx]); // tell it what plane to slice with
+		reslice_arr[plane_idx]->SetInterpolationModeToLinear();
+		reslice_arr[plane_idx]->Update();
 
 		// Create an actor for the image. You'll notice we skipped the mapper step. This is because
 		// image actors have a default mapper we can use as is if we don't want to change 
 		// the colormap etc.
-		iactor = vtkSmartPointer<vtkImageActor>::New();
+		iactor_arr[plane_idx] = vtkSmartPointer<vtkImageActor>::New();
 
 		// Connect the reslice filter to the mapper
-		iactor->GetMapper()->SetInputConnection(reslice->GetOutputPort());
+		iactor_arr[plane_idx]->GetMapper()->SetInputConnection(reslice_arr[plane_idx]->GetOutputPort());
 
 		// Renderer
-		renderer = vtkSmartPointer<vtkRenderer>::New();
-		renderer->AddActor(iactor); // Add the actor to the renderer
+		renderer_arr[plane_idx] = vtkSmartPointer<vtkRenderer>::New();
+		renderer_arr[plane_idx]->AddActor(iactor_arr[plane_idx]); // Add the actor to the renderer
 
 		// Add the renderer to the render window
-		window_x->AddRenderer(renderer);
+		window_arr[plane_idx]->AddRenderer(renderer_arr[plane_idx]);
 
 		// Render (display the image)
-		window_x->Render();
+		window_arr[plane_idx]->Render();
 
 		// Ensure we aren't clipping any of the image (cameras have a front and back plane that 
 		// clips for performance)
-		renderer->ResetCameraClippingRange();
+		renderer_arr[plane_idx]->ResetCameraClippingRange();
+
+		int map[] = { -1, 2, 1, 0 };
 
 		// Tell our slider widget what the min/max slice numbers are
-		slider_x->setRange(0, dims[2] - 1);
+		slider_arr[plane_idx]->setRange(0, dims[map[plane_idx]] - 1);
 
-
-		cout << "finished loading data\n";
-		is_data_loaded = true;
-
-	}
-
-	// TODO: refactor code; create general load_DICOM_image fxn
-	void load_DICOM_image_y(QDir dicom_dir) {
-
-		is_data_loaded = false;
-		cout << "loading data\n";
-
-		//QDir dicom_dir = choose_directory();
-
-		// Read all the DICOM files in the specified directory.
-		vtkSmartPointer<vtkDICOMImageReader> reader = vtkSmartPointer<vtkDICOMImageReader>::New();
-
-		// TODO: figure out a better way to convert QString to a char * to pass to SetDirectoryName
-		reader->SetDirectoryName(dicom_dir.absolutePath().toStdString().c_str());
-
-		// Force update, since we need to get information about the data dimensions
-		reader->Update();
-
-		int* dims = reader->GetOutput()->GetDimensions(); // Get the data dimensions
-		double* range = reader->GetOutput()->GetScalarRange(); // Get the range of intensity values
-
-		// Create a matrix which defines a plane. This plane will be used to "slice" the data
-		static double axial[16] = {
-		1, 0, 0, 0,
-		0, 1, 0, 0,
-		0, 0, 1, 0,
-		0, 0, 0, 1 };
-
-		reslice_axes_y = vtkSmartPointer<vtkMatrix4x4>::New();
-		reslice_axes_y->DeepCopy(axial);
-
-		// vtkImageReslice is the filter that does the slicing (slices a 3D dataset to become 2D)
-		reslice_y = vtkSmartPointer<vtkImageReslice>::New();
-		reslice_y->SetInputConnection(reader->GetOutputPort()); // connect the reader to this filter
-		reslice_y->SetOutputDimensionality(2);
-		reslice_y->SetResliceAxes(reslice_axes_y); // tell it what plane to slice with
-		reslice_y->SetInterpolationModeToLinear();
-		reslice_y->Update();
-
-		// Create an actor for the image. You'll notice we skipped the mapper step. This is because
-		// image actors have a default mapper we can use as is if we don't want to change 
-		// the colormap etc.
-		iactor_y = vtkSmartPointer<vtkImageActor>::New();
-
-		// Connect the reslice filter to the mapper
-		iactor_y->GetMapper()->SetInputConnection(reslice_y->GetOutputPort());
-
-		// Renderer
-		renderer_y = vtkSmartPointer<vtkRenderer>::New();
-		renderer_y->AddActor(iactor_y); // Add the actor to the renderer
-
-		// Add the renderer to the render window
-		window_y->AddRenderer(renderer_y);
-
-		// Render (display the image)
-		window_y->Render();
-
-		// Ensure we aren't clipping any of the image (cameras have a front and back plane that 
-		// clips for performance)
-		renderer_y->ResetCameraClippingRange();
-
-		// Tell our slider widget what the min/max slice numbers are
-		slider_y->setRange(0, dims[2] - 1);
-
-
-		cout << "finished loading data\n";
+		cout << "finished loading data\n\n";
 		is_data_loaded = true;
 
 	}
@@ -398,16 +334,8 @@ public:
 		renderer->ResetCamera();
 
 		// render window
-		window_volume->AddRenderer(renderer);
-		window_volume->Render();
-
-		// interactor
-		/*vtkSmartPointer<vtkRenderWindowInteractor> iren =
-			vtkSmartPointer<vtkRenderWindowInteractor>::New();
-		iren->SetRenderWindow(window_volume);*/
-
-		// start interactor
-		//iren->Start();
+		window_arr[0]->AddRenderer(renderer);
+		window_arr[0]->Render();
 
 		cout << "finished loading data\n";
 		is_data_loaded = true;
@@ -415,60 +343,48 @@ public:
 
 public slots:
 
-	/*void load_DICOM() {
-		QDir dicom_dir = choose_directory();
-	}*/
 
 	void load_DICOM() {
-		//QDir dicom_dir = choose_directory();
-		QDir dicom_dir = QDir("../data");
-		load_DICOM_image_x(dicom_dir);
-		load_DICOM_image_y(dicom_dir);
+
+		// QDir dicom_dir = choose_directory();
+		QDir dicom_dir = QDir("../data_bu");
+
+		load_DICOM_image(dicom_dir, AXIAL);
+		load_DICOM_image(dicom_dir, CORONAL);
+		load_DICOM_image(dicom_dir, SAGITTAL);
 		load_DICOM_volume(dicom_dir);
 	}
 
 
-	void sliderx_changed(int value) {
+	void slider_changed(int value) {
 
 		if (!is_data_loaded) {
 			cout << "data not loaded yet!\n";
 			return;
 		}
 
-		// Set the slice
-		reslice_axes->SetElement(2, 3, value);
-		reslice->Modified();
+		QObject* caller = sender();
+		int plane_idx;
 
-		// Update the slice label
-		label_slice_x->setText(QString::number(value));
+		if (caller == slider_arr[AXIAL])
+			plane_idx = 1;
+		else if (caller == slider_arr[CORONAL])
+			plane_idx = 2;
+		else if (caller == slider_arr[SAGITTAL])
+			plane_idx = 3;
 
-		// Re-render the image data
-		window_x->Render();
-
-		// Convert int value to QString type, expected by setText
-		//label->setText(QString::number(value));
-
-	}
-
-	void slidery_changed(int value) {
-
-		if (!is_data_loaded) {
-			cout << "data not loaded yet!\n";
-			return;
-		}
+		int map[] = { -1, 2, 1, 0 };
 
 		// Set the slice
-		reslice_axes_y->SetElement(2, 3, value);
-		reslice_y->Modified();
+		reslice_axes_arr[plane_idx]->SetElement(map[plane_idx], 3, value);
+		reslice_arr[plane_idx]->Modified();
 
 		// Update the slice label
-		label_slice_y->setText(QString::number(value));
+		slider_label_arr[plane_idx]->setText(QString::number(value));
 
 		// Re-render the image data
-		window_y->Render();
+		window_arr[plane_idx]->Render();
 
-		// Convert int value to QString type, expected by setText
-		//label->setText(QString::number(value));
 
 	}
 
