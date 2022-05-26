539ff61e0b8dea2d82d0b9571d01e8b7779f7425
Musab Shakeel <musabshakeel@gmail.com>
2022-05-26 11:54:25 -0400

    fixed small typo

--- src/ui.h
+++ src/ui.h
@@ -572,7 +572,7 @@ public slots:
 		load_DICOM_volume(dicom_dir, 2);
 
 		opacity_label1->setText("Slice Opacity: " + QString::number(DSET2_OPACITY*100));
-		opacity_slider0->setValue(DSET2_OPACITY * 100); 
+		opacity_slider1->setValue(DSET2_OPACITY * 100); 
 	}
 
 
